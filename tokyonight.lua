3f14106d841c549cedbf0570670d3beac01586d5
limonkahacker-ux <limonkahacker@gmail.com>
2025-10-05 23:09:59 +0300

    Add files via upload

---
+++ tokyonight.lua
@@ -0,0 +1,17 @@
+local VirtualInputManager = game:GetService("VirtualInputManager")
+
+local notes = {
+    "z", "x", "c", "v", "b", "v", "c", "x",
+    "g", "h", "j", "k", "l", "k", "j", "h",
+    "t", "y", "u", "i", "o", "i", "u", "y",
+    "g", "h", "j", "k", "l", "k", "j", "h"
+}
+
+local delay = 0.2
+
+for _, key in ipairs(notes) do
+    VirtualInputManager:SendKeyEvent(true, key, false, game)
+    wait(0.05)
+    VirtualInputManager:SendKeyEvent(false, key, false, game)
+    wait(delay)
+end
